888a183df101b2b2749f3becc37bb02c5a5cc0a3
= <BR0374078268@enel.com>
2021-09-20 21:51:22 -0300

    Criando projetos Core, Mobile e Web . . .

---
+++ src/DotNet6ToDo/DotNet6ToDo.Mobile/DotNet6ToDo.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace DotNet6ToDo.Mobile
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
